4d447c9e0d8126ce59a2e004e08a1b662a26d130
Christian Dillard <70979511+cdillard49@users.noreply.github.com>
2021-11-30 15:14:56 -0500

    work

--- smart_question_dil.py
+++ smart_question_dil.py
@@ -124,7 +124,7 @@ print(x_train.columns[selector.get_support(indices=True)])
 
 # 7 features here are important ones for the selection process.
 # We'll create a list of these and put them in our model.
-important=["Customer Type", "Type of Travel", "Class", "Flight Distance", "Inflight wifi service", "Online boarding", "Seat comfort"]
+important=["Customer Type", "Type of Travel", "Class", "Flight Distance", "Inflight wifi service", "Online boarding", "Seat comfort", "Inflight entertainment", "On-board service", "Leg room service"]
 df_train_important = df_train[important]
 df_test_important = df_test[important]
 
@@ -141,6 +141,7 @@ from sklearn.metrics import roc_auc_score, roc_curve
 #train=df_train[:df_train_len]
 #x_train=train.drop(labels="satisfaction",axis=1) # df with only satisfaction and df with every other variable
 #y_train=train["satisfaction"]
+
 x_train_model,x_test_model,y_train_model,y_test_model=train_test_split(df_train_important,y_train,test_size=0.33,random_state=42)
 print("x_train_model",len(x_train_model))
 print("x_test_model",len(x_test_model))
@@ -157,6 +158,17 @@ print("test",len(test))
 # Initiate the Logistic Regression Model and print the accuracy
 logreg=LogisticRegression()
 logreg.fit(x_train_model,y_train_model)
+
+importance=logreg.coef_[0]
+for i,v in enumerate(importance):
+	print('Feature: %0d, Score: %.5f' % (i,v))
+# plot feature importance
+plt.bar([x for x in range(len(importance))], importance)
+plt.title('Bar plot of feature importance')
+plt.xlabel("Features")
+plt.ylabel("Coefficients")
+plt.show()
+
 acc_log_train=round(logreg.score(x_train_model,y_train_model)*100,2)
 acc_log_test=round(logreg.score(x_test_model,y_test_model)*100,2)
 print("Training Accuracy: % {}".format(acc_log_train))
@@ -180,8 +192,8 @@ print('Logistic: ROC AUC=%.3f' % (lr_auc))
 ns_fpr, ns_tpr, _ = roc_curve(y_test_model, ns_probs)
 lr_fpr, lr_tpr, _ = roc_curve(y_test_model, lr_probs)
 # plot the roc curve for the model
-plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
-plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
+plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill: ROC AUC=%.3f'%(ns_auc))
+plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic: ROC AUC=%.3f' % (lr_auc))
 # axis labels
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
@@ -191,8 +203,8 @@ plt.legend()
 plt.show()
 # Summary page for the model
 import statsmodels.api as sm
+from sklearn.metrics import classification_report
 logit_model=sm.Logit(y_train_model,x_train_model)
 result=logit_model.fit()
 print(result.summary())
-
 
